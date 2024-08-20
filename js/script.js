e323e963a594414b5f6603b3251c443cd7515ac9
Rian Riana <rianrianabkm@gmail.com>
2024-08-20 17:25:18 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
-// script.js
-function calculateBMI(event) {
+// script.js adalah tempat memadukan tampilan dengan rumus logika yang dibutuhkan agar tampilan lebih dinamis
+function calculatebmi(event) {
 
-
+ // fuction bertugas memberikan fungsi pengoprasian pada objek yang dikaitkan dari html
     var height = document.getElementById('height').value / 100;
     var weight = document.getElementById('weight').value;
 
-    console.log()
+    console.log() // debugging untuk jaga jaga kemungkinan bug
     
-    if (height > 0 && weight > 0) {
+    if (height > 0 && weight > 0) { //menggunakan rumus if else untuk merumuskan kalkulasi dan langkah prediktif lainnya
         var bmi = weight / (height * height);
         var resultText = '';
         var categoryText = '';
 
         if (bmi < 18.5) {
-            categoryText = 'kekurangan berat badan';
-            resultText = 'Berat Badan Kurang';
+            categoryText = 'berat badan dibawah ideal';
+            resultText = 'Kekurangan Berat Badan';
         } else if (bmi < 24.9) {
-            categoryText = 'Berat Badan Normal';
-            resultText = 'Berat Badan Normal';
+            categoryText = 'Berat badan ideal';
+            resultText = 'Ideal';
         } else if (bmi < 29.9) {
             categoryText = 'berat badan berlebih';
-            resultText = 'Berat Badan Lebih';
+            resultText = 'Kelebihan Berat Badan';
         } else {
-            categoryText = 'Berat Badan Obesitas';
-            resultText = 'Berat Badan Obesitas';
+            categoryText = 'Berat obesitas';
+            resultText = 'Obesitas';
         }
         
         
         
-        document.getElementById('bmi-result').innerHTML = 'Hasil ' + resultText;
+        document.getElementById('bmi-result').innerHTML = 'Kamu ' + resultText;
         document.getElementById('bmi-value').innerHTML = bmi.toFixed(2);
-        document.getElementById('bmi-category').innerHTML = 'Anda memiliki ' + categoryText;
+        document.getElementById('bmi-category').innerHTML = 'Kamu memiliki ' + categoryText;
     } else {
         document.getElementById('bmi-value').innerHTML = '';
         document.getElementById('bmi-category').innerHTML = 'Masukkan tinggi dan berat badan yang valid.';
@@ -38,23 +38,23 @@ function calculateBMI(event) {
 }
 
 
-function resetBMI() {
-    document.getElementById('height').value = '';
+function resetbmi() {
+    document.getElementById('height').value = ''; // document.getElemetById bertugas mengaitkan element berdasarkan id yang dibuat di html
     document.getElementById('age').value = '';
     document.getElementById('weight').value = '';
     document.getElementById('bmi-result').innerText = '';
     document.getElementById('bmi-value').innerText = '';
     document.getElementById('bmi-category').innerText = '';
 }
-function showAlertKonsul() {
-    alert("Silahkan konsultasi ke dokter terdekat") ;
+function showalertkonsul() {
+    alert("Silahkan konsultasi ke dokter terdekat") ; //alert berfungsi memberikan pop-up notifikasi
 }
-function showAlertRegist() {
+function showalertregist() {
     alert("Silahkan registrasi disini") ;
 }
-function showAlertKonsulGizi() {
+function showalertkonsulgizi() {
     alert("Silahkan konsultasi Gizi disini") ;
 }
-function showAlertRegistAhli() {
+function showalertregistahli() {
     alert("Silahkan registrasi ahli disini") ;
 }
